59eed0435df5c8ae0a5b8dde2cc6eea0029dff7f
Jagepard <dankorot@gmail.com>
2020-04-29 14:20:00 +0300

    reformat

--- mediator.rb
+++ mediator.rb
@@ -17,13 +17,13 @@ class Mediator
   end
 
   def notify(listener_name, handler = nil)
-      if @listeners.has_key?(listener_name)
-          listener_class_name = @listeners[listener_name][0]
-          method_name         = @listeners[listener_name][1]
+    if @listeners.has_key?(listener_name)
+      listener_class_name = @listeners[listener_name][0]
+      method_name = @listeners[listener_name][1]
 
-          listener_class_name.send(method_name, handler)
-          return
-      end
-      raise "Listener " + listener_name + " does not exist"
+      listener_class_name.send(method_name, handler)
+      return
     end
+    raise "Listener " + listener_name + " does not exist"
+  end
 end
