01a57584f2cf4af7b2325853c3638a8f39657c7b
Jagepard <dankorot@gmail.com>
2020-04-29 12:54:54 +0300

    add Handler

---
+++ handler.rb
@@ -0,0 +1,14 @@
+=begin
+author  : Jagepard <jagepard@yandex.ru>
+license https://mit-license.org/ MIT
+=end
+
+class Handler
+  def message=(message)
+    @message = message
+  end
+
+  def message
+    @message
+  end
+end
